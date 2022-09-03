54448e457f8b24c306e60bcbdfc21ccb5a62e1a3
aheroi <knipax@gmail.com>
2022-09-03 16:47:52 +0300

    change way to register models

--- myfinpages/admin.py
+++ myfinpages/admin.py
@@ -2,6 +2,37 @@ from django.contrib import admin
 from myfinpages.models import Income, Outcome, Balance
 
 # Register your models here.
-admin.site.register(Income)
-admin.site.register(Outcome)
-admin.site.register(Balance)
+
+# admin.site.register(Income)
+# admin.site.register(Outcome)
+# admin.site.register(Balance)
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    search_fields = ('date', 'type', 'value', 'notes', 'comment_to_notes', 'created_at')
+
+    list_display = ('pk', 'date', 'type', 'value', 'notes', 'comment_to_notes', 'created_at')
+    list_display_links = ('pk', 'value', 'date', 'type', 'notes', 'comment_to_notes')
+
+    ordering = ('pk', 'value', 'date', 'type', 'notes', 'comment_to_notes', 'created_at')
+
+
+@admin.register(Outcome)
+class OutcomeAdmin(admin.ModelAdmin):
+    search_fields = ('date', 'type', 'value', 'notes', 'comment_to_notes', 'created_at')
+
+    list_display = ('pk', 'date', 'type', 'value', 'notes', 'comment_to_notes', 'created_at')
+    list_display_links = ('pk', 'value', 'date', 'type', 'notes', 'comment_to_notes')
+
+    ordering = ('pk', 'value', 'date', 'type', 'notes', 'comment_to_notes', 'created_at')
+
+
+@admin.register(Balance)
+class BalanceAdmin(admin.ModelAdmin):
+    search_fields = ('date', 'type', 'value', 'notes', 'comment_to_notes', 'created_at')
+
+    list_display = ('pk', 'date', 'type', 'value', 'notes', 'comment_to_notes', 'created_at')
+    list_display_links = ('pk', 'value', 'date', 'type', 'notes', 'comment_to_notes')
+
+    ordering = ('pk', 'value', 'date', 'type', 'notes', 'comment_to_notes', 'created_at')
