b6533c78b0d8dc7b616b023903b1c08e21098e33
Andrii <knipax@gmail.com>
2022-10-22 16:58:31 +0300

    added active in sidebar

--- website/views.py
+++ website/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    # return render(request, 'base.html')
     return render(request, 'website/index.html')
 
 
+def get_page_path(request):
+    return {
+        'page_path': request.path,
+    }
